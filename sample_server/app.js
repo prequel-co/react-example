8e998de23ae701aa4a75e5750ec937949665496b
Wesley Herts <wesley@prequel.co>
2024-05-13 16:50:41 -0400

    Add comment explaining Recipient ID validation on server side

--- sample_server/app.js
+++ sample_server/app.js
@@ -9,7 +9,7 @@ const API_KEY = process.env.API_KEY;
 const PREQUEL_HOST = process.env.REACT_APP_PREQUEL_HOST;
 const REACT_APP_PORT = process.env.REACT_APP_PORT ?? 3000;
 const NODE_SERVER_PORT = process.env.REACT_APP_NODE_SERVER_PORT ?? 9999;
-const RECIPIENT_ID = process.env.REACT_APP_RECIPIENT_ID;
+const ENV_RECIPIENT_ID = process.env.REACT_APP_RECIPIENT_ID;
 const app = express();
 
 // Allow all origins
@@ -18,11 +18,17 @@ app.use(express.json());
 
 const AUTH_TOKEN = "/auth-token"
 
-const REACT_EXAMPLE_ORIGIN = `localhost:${REACT_APP_PORT}`
+const REACT_EXAMPLE_ORIGIN = `http://localhost:${REACT_APP_PORT}`
 const GENERATE_AUTH_TOKEN = `${PREQUEL_HOST}/actions/generate-scoped-auth-token`;
 
 app.post(AUTH_TOKEN, async (req, res) => {
   console.log(`POST Request received: ${AUTH_TOKEN}`)
+
+  // This Recipient ID value is pulled from the environment for the purpose of this example.
+  // When implementing against the Prequel API, you should set this value in your backend
+  // using a server-side token or your preferred method to associate user sessions with Recipient ID values.
+  // This Recipient ID value should not be accepted from the frontend without validation.
+  const recipientId = ENV_RECIPIENT_ID;
 
   const scopedToken = await fetch(GENERATE_AUTH_TOKEN, {
     method: "POST",
@@ -33,7 +39,7 @@ app.post(AUTH_TOKEN, async (req, res) => {
     },
     body: JSON.stringify({
       application_origin: REACT_EXAMPLE_ORIGIN,
-      recipient_id: RECIPIENT_ID,
+      recipient_id: recipientId, // This is a validated Recipient ID value
       destination: req.body,
     }),
   }).then((response) => response.json())
